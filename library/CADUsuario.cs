0d1fcd2d17cd3366710e882b25df394938251803
David <davidmolinamar@gmail.com>
2020-03-22 14:02:59 +0100

    74378460W Creadas las clases CADUsuario y ENUsuario además de la base de datos.

---
+++ library/CADUsuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class CADUsuario
+    {
+        private string constring;
+        public CADUsuario() { constring = null; }
+        public bool createUsuario(ENUsuario en) { return false; }
+        public bool readUsuario(ENUsuario en) { return false; }
+        public bool readFirstUsuario(ENUsuario en) { return false; }
+        public bool readNextUsuario(ENUsuario en) { return false; }
+        public bool readPrevUsuario(ENUsuario en) { return false; }
+        public bool updateUsuario(ENUsuario en) { return false; }
+        public bool deleteUsuario(ENUsuario en) {
+            SqlConnection conn = null;
+            string comando = "Delete from Usuario where nif = " + en.nif;
+            try
+            {
+                conn = new SqlConnection(constring);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ library/ENUsuario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class ENUsuario
+    {
+        private string pnif;
+        private string pnombre;
+        private int pedad;
+
+        public string nif {
+            get => pnif;
+            set
+            {
+                pnif = value;
+            }
+        }
+        public string nombre
+        {
+            get => pnombre;
+            set
+            {
+                pnombre = value;
+            }
+        }
+        public int edad {
+            get => edad;
+            set {
+                if(value>=0)
+                    edad = value;                    
+            }
+        }
+        public ENUsuario() {
+            nif = null;
+            nombre = null;
+            edad = 0;
+        }
+        public ENUsuario(string nif, string nombre, int edad){
+            this.nif = nif;
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        public bool createUsuario() { return false; }
+        public bool readUsuario() { return false; }
+        public bool readFirstUsuario() {
+            this=
+            return false; }
+        public bool readNextUsuario() { return false; }
+        public bool readPrevUsuario() { return false; }
+        public bool updateUsuario() { return false; }
+        public bool deleteUsuario() { return false; }
+    }
+}
