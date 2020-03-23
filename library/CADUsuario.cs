dd8926148632434d92217441cc718810bc533bca
David <davidmolinamar@gmail.com>
2020-03-23 09:45:05 +0100

    74378460W realizados delete read y update

--- library/CADUsuario.cs
+++ library/CADUsuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,27 @@ namespace library
     public class CADUsuario
     {
         private string constring;
-        public CADUsuario() { constring = null; }
-        public bool createUsuario(ENUsuario en) { return false; }
-        public bool readUsuario(ENUsuario en) { return false; }
-        public bool readFirstUsuario(ENUsuario en) { return false; }
-        public bool readNextUsuario(ENUsuario en) { return false; }
-        public bool readPrevUsuario(ENUsuario en) { return false; }
-        public bool updateUsuario(ENUsuario en) { return false; }
+        public CADUsuario() {
+            constring = null; 
+        }
+        public bool createUsuario(ENUsuario en) {
+            return false;
+        }
+        public bool readUsuario(ENUsuario en) { 
+             
+        }
+        public bool readFirstUsuario(ENUsuario en) { 
+            return false; 
+        }
+        public bool readNextUsuario(ENUsuario en) { 
+            return false; 
+        }
+        public bool readPrevUsuario(ENUsuario en) { 
+            return false;
+        }
+        public bool updateUsuario(ENUsuario en) { 
+            return false;
+        }
         public bool deleteUsuario(ENUsuario en) {
             SqlConnection conn = null;
             string comando = "Delete from Usuario where nif = " + en.nif;
@@ -25,13 +40,17 @@ namespace library
                 conn = new SqlConnection(constring);
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
                 return true;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
+                conn.Close();
                 return false;
             }
-            
+            finally {
+                conn.Close();
+            }
         }
     }
 }
