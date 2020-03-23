076a01c0ec4ea466788bc115518a3c1c1f0f7f0f
David <davidmolinamar@gmail.com>
2020-03-23 10:44:25 +0100

    74378460W Completado CADUsuario

--- library/CADUsuario.cs
+++ library/CADUsuario.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace library
 {
@@ -12,25 +15,188 @@ namespace library
     {
         private string constring;
         public CADUsuario() {
-            constring = null; 
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
         }
         public bool createUsuario(ENUsuario en) {
+            SqlConnection conn = new SqlConnection(constring);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("Insert Into usuario (nif, nombre, edad) " +
+                    "VALUES (" + en.nif + ", " + en.nombre + ", " + en.edad+")", conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return true;
+        }
+        public bool readUsuario(ENUsuario en) {
+            SqlConnection conn = new SqlConnection(constring);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * " +
+                                                        "FROM usuario where nif = "+en.nif+" " +
+                                                        "and nombre = "+en.nombre +
+                                                        " and edad = "+ en.edad, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    if (dr["nif"].ToString() == en.nif.ToString())
+                    {
+                        en.edad = int.Parse(dr["edad"].ToString());
+                        en.nombre = dr["nombre"].ToString();
+                        en.nif = dr["nif"].ToString();
+                        conn.Close();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
             return false;
         }
-        public bool readUsuario(ENUsuario en) { 
-             
+        public bool readFirstUsuario(ENUsuario en) {
+            SqlConnection conn = new SqlConnection(constring);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * " +
+                                                        "FROM usuario where nif = " + en.nif + " " +
+                                                        "and nombre = " + en.nombre +
+                                                        " and edad = " + en.edad, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                en.edad = int.Parse(dr["edad"].ToString());
+                en.nombre = dr["nombre"].ToString();
+                en.nif = dr["nif"].ToString();
+                conn.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
-        public bool readFirstUsuario(ENUsuario en) { 
-            return false; 
-        }
-        public bool readNextUsuario(ENUsuario en) { 
-            return false; 
-        }
-        public bool readPrevUsuario(ENUsuario en) { 
+        public bool readNextUsuario(ENUsuario en) {
+            SqlConnection conn = new SqlConnection(constring);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * " +
+                                                        "FROM usuario where nif = " + en.nif + " " +
+                                                        "and nombre = " + en.nombre +
+                                                        " and edad = " + en.edad, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                bool encontrado = false;
+                while (dr.Read())
+                {
+                    if (encontrado)
+                    {
+                        en.edad = int.Parse(dr["edad"].ToString());
+                        en.nombre = dr["nombre"].ToString();
+                        en.nif = dr["nif"].ToString();
+                        conn.Close();
+                        return true;
+                    }
+                    else if (dr["nif"].ToString() == en.nif.ToString())
+                    {
+                        encontrado = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
             return false;
         }
-        public bool updateUsuario(ENUsuario en) { 
+        public bool readPrevUsuario(ENUsuario en) {
+            SqlConnection conn = new SqlConnection(constring);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * " +
+                                                        "FROM usuario where nif = " + en.nif + " " +
+                                                        "and nombre = " + en.nombre +
+                                                        " and edad = " + en.edad, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                ENUsuario enaux = new ENUsuario();
+                while (dr.Read())
+                {
+                    if (dr["nif"].ToString() == en.nif.ToString())
+                    {
+                        en.edad = enaux.edad;
+                        en.nombre = enaux.nombre;
+                        en.nif = enaux.nif;
+                        return true;
+                    }
+                    else {
+                        enaux.edad = int.Parse(dr["edad"].ToString());
+                        enaux.nombre = dr["nombre"].ToString();
+                        enaux.nif = dr["nif"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
             return false;
+        }
+        public bool updateUsuario(ENUsuario en) {
+            SqlConnection conn = new SqlConnection(constring);
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Usuario " +
+                                                    "SET nif = '" + en.nif + "', nombre = '" + en.nombre + "',  edad = '" + en.edad + 
+                                                    "where nif ="+ en.nif);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return true;
         }
         public bool deleteUsuario(ENUsuario en) {
             SqlConnection conn = null;
@@ -41,9 +207,8 @@ namespace library
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(comando, conn);
                 cmd.ExecuteNonQuery();
-                return true;
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 conn.Close();
                 return false;
@@ -51,6 +216,7 @@ namespace library
             finally {
                 conn.Close();
             }
+            return true;
         }
     }
 }
